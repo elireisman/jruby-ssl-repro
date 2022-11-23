a5899099f8d7593cdcb93b4382b2f3d2868aad71
Eli Reisman <elireisman@github.com>
2022-11-22 19:33:22 -0800

    fetch props file

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require "open-uri"
 
 def main
-  url = "https://repo1.maven.org/maven2/.index/"
+  url = "https://repo1.maven.org/maven2/.index/nexus-maven-repository-index.properties"
   remote_io = open(url, "User-Agent" => "Testing")
   IO.copy_stream(remote_io, STDOUT)
   remote_io.close
